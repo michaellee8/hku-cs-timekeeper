af981fe0888eb797f3c7bcfcba5b5c3e78ca90b9
michaellee8 <ckmichael8@gmail.com>
2018-09-30 21:07:49 +0800

    Implemented step 1

--- main.c
+++ main.c
@@ -2,11 +2,43 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <stdlib.h>
+#include <linux/limits.h>
+#include <errno.h>
 
-int main(int argc, char *argv[])
-{
-    int total_level;
-    sscanf(argv[1], "%d", &total_level);
-    create_process(total_level - 1);
+
+void handle_process_created(pid_t pid, char *cmd) {
+    printf("Process %d created with command %\n", (int) pid, cmd);
+    int status;
+    waitpid(pid, &status, 0)
+}
+
+void run_command(char *argv[]) {
+    pid_t pid;
+    int status;
+    if (*argv[0] == '/' || *argv[0] == '.') {
+        char executable_path[PATH_MAX];
+        realpath(argv[0], executable_path);
+        pid = fork();
+        if (pid == 0) {
+            int errno = execv(executable_path, argv);
+        } else {
+            printf("Process %d created with command %\n", (int) pid, argv[0]);
+            waitpid(pid, &status, 0);
+        }
+    } else {
+        pid = fork();
+        if (pid == 0) {
+            int errno = execvp(argv[0], argv);
+        } else {
+            printf("Process %d created with command %s\n", (int) pid, argv[0]);
+            waitpid(pid, &status, 0);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    run_command(argv + 1);
 }
